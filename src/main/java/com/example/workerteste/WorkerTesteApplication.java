726e67669bb70747a7d82dfdb3f039f2866c3c5a
CaduhPavei <caduhpavei@gmail.com>
2024-04-03 20:02:20 -0300

    First commit

---
+++ src/main/java/com/example/workerteste/WorkerTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.workerteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workerteste/WorkerTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workerteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
